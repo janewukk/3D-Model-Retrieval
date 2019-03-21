ee00cdfec9f827abbabd875b2f5fc34516fdf8e5
Junyue Wu <junyuewu@Junyues-MacBook-Pro.local>
2019-03-21 16:43:23 -0700

    some stuff

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
