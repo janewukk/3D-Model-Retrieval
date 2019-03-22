76033b7d2dc52b44a94c80ad369fd85e1d7949ef
Junyue Wu <junyuewu@Junyues-MacBook-Pro.local>
2019-03-21 21:15:10 -0700

    UI feature almost done

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
+import json
+from base64 import b64decode
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return render_template('index.html')
+	return render_template('index.html')
+
+def query(filename):
+	f = open(filename)
+	## to be implemented
+
+@app.route("/draw", methods=['POST'])
+def process_draw_data():
+	data = json.loads(request.data)
+	keyword = data['keyword']
+	image = data['image']
+	header, encoded = image.split(",", 1)
+	image_content = b64decode(encoded)
+	with open("query/query.png", "wb") as f:
+		f.write(image_content)
+
+	query("query/query.png")
+	data_back = {
+		"image": [],
+		"rank": []
+	}
+	return json.dumps(data_back)
+
+@app.route('/upload', methods=['POST'])
+def process_upload_data():
+	return "good"
--- static/js/index.js
+++ static/js/index.js
@@ -4,11 +4,36 @@ $(document).ready(function(){
 	var canvas = document.getElementById('sketcher');
 	atra = atrament(canvas,  canvas.offsetWidth, canvas.offsetHeight);
 	var clearButton = document.getElementById('clear');
+
+
+	var link = document.createElement('a');
+    link.innerHTML = 'Query';
+    link.id = "query";
+    link.style = "margin-top:-10px";
+	link.addEventListener('click', function(ev) {
+		var canvas = document.getElementById('sketcher');
+		var img = canvas.toDataURL("image/png");
+	    link.href = canvas.toDataURL();
+	    var data = {
+			"keyword": "",
+			"image": img
+		}
+	    $.ajax({
+			type : 'POST',
+			url : "/draw",
+			contentType: 'application/json',
+			data : JSON.stringify(data),
+			success: function(data){
+			    data = JSON.parse(data)
+			    console.log(data)
+			  }
+		});
+	}, false);
+	document.getElementById("div_download").appendChild(link);
+	$('#query').addClass("btn btn-success");
 });
 
 function clear()
 {
-	// event.preventDefault(); 
 	console.log("onclick")
-	// atra.clear();
 }
